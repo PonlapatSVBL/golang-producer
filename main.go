1f2252ebf5c967e9d030f858ad36916b052c02d7
human.soft <human.soft@hotmail.com>
2024-07-18 00:36:31 +0700

    (piw) mysql struct & print json

--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
-	"github.com/PonlapatSVBL/golang-producer/database/mysql"
-	service "github.com/PonlapatSVBL/golang-producer/services"
+	"github.com/PonlapatSVBL/golang-producer/connections/mysql"
 	"github.com/PonlapatSVBL/golang-producer/utils"
 )
 
 func main() {
 	utils.LoadEnv()
-	mysql.Connect()
 
-	employees, err := service.GetEmployeeByServerId([]string{"1", "2", "3"})
-	if err != nil {
-		log.Fatal(err)
+	type Employee struct {
+		EmployeeId   string `json:"employee_id" db:"employee_id"`
+		EmployeeName string `json:"employee_name" db:"employee_name"`
 	}
 
-	utils.PrintFilteredJsonMarshal(employees)
+	query := "SELECT employee_id, employee_name FROM hms_api.comp_employee LIMIT 1,2"
+
+	mysqlInstance := mysql.NewMysql()
+	employees := []Employee{}
+	mysqlInstance.SqlList(&employees, query)
+
+	utils.PrintJsonIndent(employees, true)
+	fmt.Println()
 }
