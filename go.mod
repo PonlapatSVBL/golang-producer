f8d221815b737ad55b968e7e22a93756a1f0e9c9
Ponlapat <human.soft@hotmail.com>
2024-07-18 20:53:42 +0700

    send messagae

--- go.mod
+++ go.mod
@@ -8,7 +8,15 @@ require (
 )
 
 require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.11.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.7.0 // indirect
+	github.com/Azure/go-amqp v1.0.5 // indirect
+	golang.org/x/net v0.24.0 // indirect
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus v1.7.1
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/jmoiron/sqlx v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,71 @@
 package main
 
 import (
-	employeeService "github.com/PonlapatSVBL/golang-producer/services/employee"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
 	"github.com/PonlapatSVBL/golang-producer/utils"
+)
+
+var (
+	connectionString string
+	queueName        string
 )
 
 func main() {
 	utils.LoadEnv()
 
-	employees, _ := employeeService.GetEmployeeByServerId([]string{"1", "2"})
-	utils.PrintExistJsonIndent(employees, true)
+	// employees, _ := employeeService.GetEmployeeByServerId([]string{"1", "2"})
+	// utils.PrintExistJsonIndent(employees, true)
 
 	// utils.ExportModel("hms_api.comp_employee")
+
+	runProducer()
+}
+
+func runProducer() {
+	connectionString = os.Getenv("CONNECTION_STRING")
+	queueName = os.Getenv("QUEUE_NAME")
+
+	// สร้าง Service Bus Client
+	client, err := azservicebus.NewClientFromConnectionString(connectionString, nil)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Create a sender for the queue
+	sender, err := client.NewSender(queueName, nil)
+	if err != nil {
+		log.Fatalf("Failed to create Service Bus Sender: %v", err)
+	}
+
+	// Create a message to send
+	sessionID := "session-id-123"
+	message := &azservicebus.Message{
+		Body:      []byte("Hello, Service Bus!"),
+		SessionID: &sessionID,
+	}
+
+	// Send the message
+	err = sender.SendMessage(context.TODO(), message, nil)
+	if err != nil {
+		log.Fatalf("Failed to send message: %v", err)
+	}
+
+	fmt.Println("Message send successfully")
+
+	// Close the sender
+	err = sender.Close(context.TODO())
+	if err != nil {
+		log.Fatalf("Failed to close sender: %v", err)
+	}
+
+	// Close the client
+	err = client.Close(context.TODO())
+	if err != nil {
+		log.Fatalf("Failed to close client: %v", err)
+	}
 }
