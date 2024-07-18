f1626fea9d05afede22ab4d05f747aa90d02ad4e
human.soft <human.soft@hotmail.com>
2024-07-19 00:37:31 +0700

    producer send message concurrent

--- main.go
+++ main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
 	"github.com/PonlapatSVBL/golang-producer/utils"
 )
 
+type MessagePayload struct {
+	ID int `json:"id"`
+}
+
 var (
 	connectionString string
 	queueName        string
+	maxProducer      int = 5
 )
 
 func main() {
@@ -30,6 +37,8 @@ func runProducer() {
 	connectionString = os.Getenv("CONNECTION_STRING")
 	queueName = os.Getenv("QUEUE_NAME")
 
+	messages := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
 	// สร้าง Service Bus Client
 	client, err := azservicebus.NewClientFromConnectionString(connectionString, nil)
 	if err != nil {
@@ -42,7 +51,7 @@ func runProducer() {
 		log.Fatalf("Failed to create Service Bus Sender: %v", err)
 	}
 
-	// Create a message to send
+	/* // Create a message to send
 	sessionID := "session-id-123"
 	message := &azservicebus.Message{
 		Body:      []byte("Hello, Service Bus!"),
@@ -53,7 +62,8 @@ func runProducer() {
 	err = sender.SendMessage(context.TODO(), message, nil)
 	if err != nil {
 		log.Fatalf("Failed to send message: %v", err)
-	}
+	} */
+	sendConcurrentMessage(sender, messages)
 
 	fmt.Println("Message send successfully")
 
@@ -68,4 +78,54 @@ func runProducer() {
 	if err != nil {
 		log.Fatalf("Failed to close client: %v", err)
 	}
+}
+
+func sendConcurrentMessage(sender *azservicebus.Sender, messages []int) {
+	var wg sync.WaitGroup
+
+	queueMessage := make(chan int, len(messages))
+
+	for i := 0; i < maxProducer; i++ {
+		wg.Add(1)
+
+		sessionID := fmt.Sprintf("SID-%d", i)
+
+		go func(i int, sessionID string) {
+			defer wg.Done()
+
+			for msg := range queueMessage {
+				payload := MessagePayload{
+					ID: msg,
+				}
+
+				body, err1 := json.Marshal(payload)
+				if err1 != nil {
+					log.Fatalf("Failed to marshal JSON: %v", err1)
+				}
+				// body = []byte(strconv.Itoa(msg))
+
+				message := &azservicebus.Message{
+					Body:      body,
+					SessionID: &sessionID,
+				}
+
+				fmt.Printf("Producer-%d is sending => SessionId: %s, Message: %d\n", i, sessionID, msg)
+
+				err := sender.SendMessage(context.TODO(), message, nil)
+				if err != nil {
+					log.Fatalf("Failed to send message: %v", err)
+				}
+				// time.Sleep(1 * time.Second)
+
+				fmt.Printf("Producer-%d is send success => SessionId: %s, Message: %d\n", i, sessionID, msg)
+			}
+		}(i, sessionID)
+	}
+
+	for _, msg := range messages {
+		queueMessage <- msg
+	}
+	close(queueMessage)
+
+	wg.Wait()
 }
